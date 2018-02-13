752036606f4431ad75e7364d539ba3c415696795
Simon Dietz <simon_atelier@web.de>
2018-02-13 01:20:48 +0100

    Fixed deny open villager inventory.

--- src/usecases/PerformAction/OpenInventory/OpenInventoryUseCase.java
+++ src/usecases/PerformAction/OpenInventory/OpenInventoryUseCase.java
@@ -2,12 +2,22 @@ package usecases.PerformAction.OpenInventory;
 
 import java.util.UUID;
 
+import context.Context;
+import game.Game;
+import game.States.WaitingGameState;
+import gateways.GameGateway;
+
 public class OpenInventoryUseCase implements OpenInventory {
 
 	@Override
 	public boolean canOpenInventory(UUID uniquePlayerId) {
-//		return !Context.gameGateway.isIngame(uniquePlayerId);
-		return true;
+		if (!Context.gameGateway.isIngame(uniquePlayerId)) {
+			return true;
+		}
+		
+		GameGateway gameGateway = Context.gameGateway;
+		Game game = gameGateway.getGameOfPlayer(uniquePlayerId);
+		return game.getGameState().getClass() == WaitingGameState.class;
 	}
 
 }
