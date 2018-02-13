5cf4b0e576573fcfd749846a05d79da206f7c26f
Simon Dietz <simon_atelier@web.de>
2018-02-13 01:32:59 +0100

    Refactoring. Extracted methods.

--- src/usecases/PerformAction/OpenInventory/OpenInventory.java
+++ src/usecases/PerformAction/OpenInventory/OpenInventory.java
@@ -2,8 +2,12 @@ package usecases.PerformAction.OpenInventory;
 
 import java.util.UUID;
 
+import gateways.GameGateway;
+
 public interface OpenInventory {
 
 	boolean canOpenInventory(UUID uniquePlayerId);
+	
+	void setGameGateway(GameGateway gameGateway);
 	
 }
--- src/usecases/PerformAction/OpenInventory/OpenInventoryUseCase.java
+++ src/usecases/PerformAction/OpenInventory/OpenInventoryUseCase.java
@@ -2,22 +2,48 @@ package usecases.PerformAction.OpenInventory;
 
 import java.util.UUID;
 
-import context.Context;
 import game.Game;
 import game.States.WaitingGameState;
 import gateways.GameGateway;
 
 public class OpenInventoryUseCase implements OpenInventory {
 
+	private UUID uniquePlayerId;
+	private Game game;
+	private GameGateway gameGateway;
+	
 	@Override
 	public boolean canOpenInventory(UUID uniquePlayerId) {
-		if (!Context.gameGateway.isIngame(uniquePlayerId)) {
+		setPlayer(uniquePlayerId);
+		
+		if (playerIsNotIngame()) {
 			return true;
 		}
 		
-		GameGateway gameGateway = Context.gameGateway;
-		Game game = gameGateway.getGameOfPlayer(uniquePlayerId);
+		findGameOfPlayer();
+		
+		return gameStateIsWaiting();
+	}
+	
+	private void findGameOfPlayer() {
+		game = gameGateway.getGameOfPlayer(uniquePlayerId);
+	}
+	
+	private boolean playerIsNotIngame() {
+		return !gameGateway.isIngame(uniquePlayerId);
+	}
+	
+	private boolean gameStateIsWaiting() {
 		return game.getGameState().getClass() == WaitingGameState.class;
+	}
+	
+	private void setPlayer(UUID uniquePlayerId) {
+		this.uniquePlayerId = uniquePlayerId;
+	}
+
+	@Override
+	public void setGameGateway(GameGateway gameGateway) {
+		this.gameGateway = gameGateway;
 	}
 
 }
