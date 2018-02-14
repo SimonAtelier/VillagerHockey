a2eff4bbbdc4f488109483895746ca8bb0c90244
Simon Dietz <simon_atelier@web.de>
2018-02-14 14:15:02 +0100

    Refactoring. Removed clutter.

--- src/game/GameListener.java
+++ src/game/GameListener.java
@@ -2,11 +2,7 @@ package game;
 
 import java.util.UUID;
 
-import game.States.GameState;
-
 public interface GameListener {
-
-//	void onGameStateChanged(Game game, GameState from, GameState to);
 
 	void onPlayerJoin(Game game, UUID player);
 	
