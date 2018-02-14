bb98cebab4a1ef7fc21d9d662750f3da2771a335
Simon Dietz <simon_atelier@web.de>
2018-02-14 09:13:14 +0100

    Moved execution of DisplayWinner use case to own controller class.

--- src/game/CountDown/Winner/WinnerCountDownController.java
+++ src/game/CountDown/Winner/WinnerCountDownController.java
@@ -5,27 +5,13 @@ import game.Game;
 import game.CountDown.CountDown;
 import game.CountDown.CountDownListener;
 import game.States.WaitingGameState;
-import usecases.DisplayWinner.DisplayWinner;
-import usecases.DisplayWinner.DisplayWinnerPresenter;
-import usecases.DisplayWinner.DisplayWinnerRequestModel;
-import usecases.DisplayWinner.DisplayWinnerUseCase;
-import usecases.DisplayWinner.DisplayWinnerView;
-import usecases.DisplayWinner.DisplayWinnerViewImpl;
-import usecases.DisplayWinner.DisplayWinner.DisplayWinnerResponse;
+import usecases.DisplayWinner.DisplayWinnerController;
 
 public class WinnerCountDownController implements CountDownListener {
 
 	@Override
 	public void onStart(String game, int timeLeftInSeconds) {
-		DisplayWinnerRequestModel requestModel = new DisplayWinnerRequestModel();
-		requestModel.setGame(game);
-		
-		DisplayWinnerView view = new DisplayWinnerViewImpl();
-		DisplayWinnerResponse presenter = new DisplayWinnerPresenter(view);
-
-		DisplayWinner useCase = new DisplayWinnerUseCase();
-		useCase.setGameGateway(Context.gameGateway);
-		useCase.execute(requestModel, presenter);
+		new DisplayWinnerController().onDisplayWinner(game);
 	}
 
 	@Override
