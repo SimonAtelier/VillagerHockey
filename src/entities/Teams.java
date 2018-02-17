24b6d1add3f3095fc1f1aa1816a1ffc5d6dbc2d4
Simon Dietz <simon_atelier@web.de>
2018-02-17 13:54:24 +0100

    Removed synchronized statements.

--- src/entities/Teams.java
+++ src/entities/Teams.java
@@ -6,8 +6,6 @@ import java.util.UUID;
 
 public class Teams {
 
-	private static final Object LIST_LOCK = new Object();
-
 	private List<Team> teams;
 
 	public Teams() {
@@ -15,11 +13,9 @@ public class Teams {
 	}
 
 	public void add(Team team) {
-		synchronized (LIST_LOCK) {
-			teams.add(team);
-		}
+		teams.add(team);
 	}
-	
+
 	public void removePlayer(UUID player) {
 		Team team = findTeamOfPlayer(player);
 		if (team != null)
@@ -27,92 +23,77 @@ public class Teams {
 	}
 
 	public void resetTeamScores() {
-		synchronized (LIST_LOCK) {
-			for (Team team : teams) {
-				team.setScore(0);
-			}
+		for (Team team : teams) {
+			team.setScore(0);
 		}
 	}
 
 	public Team findTeamByName(String name) {
-		synchronized (LIST_LOCK) {
-			for (Team team : teams) {
-				if (team.getName().equals(name)) {
-					return team;
-				}
+		for (Team team : teams) {
+			if (team.getName().equals(name)) {
+				return team;
 			}
-			return null;
 		}
+		return null;
+
 	}
 
 	public Team findTeamByColor(TeamColor color) {
-		synchronized (LIST_LOCK) {
-			for (Team team : teams) {
-				if (team.getColor().equals(color)) {
-					return team;
-				}
+		for (Team team : teams) {
+			if (team.getColor().equals(color)) {
+				return team;
 			}
-			return null;
 		}
+		return null;
 	}
 
 	public Team findTeamOfPlayer(UUID player) {
-		synchronized (LIST_LOCK) {
-			for (int i = 0; i < teams.size(); i++) {
-				Team team = teams.get(i);
-				if (team.containsPlayer(player)) {
-					return team;
-				}
+		for (int i = 0; i < teams.size(); i++) {
+			Team team = teams.get(i);
+			if (team.containsPlayer(player)) {
+				return team;
 			}
-			return null;
 		}
+		return null;
 	}
 
 	public Team findLowestTeam() {
-		synchronized (LIST_LOCK) {
-			Team lowestTeam = teams.get(0);
-			for (int i = 0; i < teams.size(); i++) {
-				Team team = teams.get(i);
-				if (team.size() < lowestTeam.size()) {
-					lowestTeam = team;
-				}
+		Team lowestTeam = teams.get(0);
+		for (int i = 0; i < teams.size(); i++) {
+			Team team = teams.get(i);
+			if (team.size() < lowestTeam.size()) {
+				lowestTeam = team;
 			}
-			return lowestTeam;
 		}
+		return lowestTeam;
 	}
-	
+
 	public Team findTeamWithHighestScore() {
-		synchronized (LIST_LOCK) {
-			int highestScore = 0;
-			Team teamWithHighestScore = null;
-			for (Team team : teams) {
-				int score = team.getScore();
-				if (score >= highestScore) {
-					highestScore = score;
-					teamWithHighestScore = team;
-				}
+		int highestScore = 0;
+		Team teamWithHighestScore = null;
+		for (Team team : teams) {
+			int score = team.getScore();
+			if (score >= highestScore) {
+				highestScore = score;
+				teamWithHighestScore = team;
 			}
-			return teamWithHighestScore;
 		}
+		return teamWithHighestScore;
 	}
 
 	public List<Team> findAllTeams() {
-		synchronized (LIST_LOCK) {
-			return new ArrayList<Team>(teams);
-		}
+		return new ArrayList<Team>(teams);
 	}
-	
+
 	public boolean equalScores() {
-		synchronized (LIST_LOCK) {
-			boolean equalScores = true;
-			int score = teams.get(0).getScore();
-			for (Team team : teams) {
-				equalScores &= (score == team.getScore());
-			}
-			return equalScores;
+		boolean equalScores = true;
+		int score = teams.get(0).getScore();
+		for (Team team : teams) {
+			equalScores &= (score == team.getScore());
 		}
+		return equalScores;
 	}
-	
+
 	public boolean containsTeamWithName(String name) {
 		return findTeamByName(name) != null;
 	}
@@ -122,19 +103,15 @@ public class Teams {
 	}
 
 	public int getMaximumAmountOfPlayers() {
-		synchronized (LIST_LOCK) {
-			int maximumAmount = 0;
-			for (Team team : teams) {
-				maximumAmount += team.getMaximumSize();
-			}
-			return maximumAmount;
+		int maximumAmount = 0;
+		for (Team team : teams) {
+			maximumAmount += team.getMaximumSize();
 		}
+		return maximumAmount;
 	}
 
 	public int getNumberOfTeams() {
-		synchronized (LIST_LOCK) {
-			return teams.size();
-		}
+		return teams.size();
 	}
 
 }
