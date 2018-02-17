43032ba1270e8c52b3bb02850077ab3b18f219cd
Simon Dietz <simon_atelier@web.de>
2018-02-17 09:46:17 +0100

    Refactoring. Extracted method.

--- src/usecases/StartGame/StartGameUseCase.java
+++ src/usecases/StartGame/StartGameUseCase.java
@@ -8,51 +8,56 @@ import gateways.Permissions;
 
 public class StartGameUseCase implements StartGame {
 
+	private Game game;
 	private StartGameRequest request;
 	private GameGateway gameGateway;
 	private PermissionGateway permissionGateway;
-	
+
 	@Override
 	public void execute(StartGameRequest request, StartGameResponse response) {
 		setRequest(request);
-		
+
 		if (noPermission()) {
 			response.onNoPermission();
 			return;
 		}
-		
+
 		if (noSuchGame()) {
 			response.onNoSuchGame(request.getGame());
 			return;
 		}
-		
-		if(isAlreadyStarted()) {
+
+		initializeGame();
+
+		if (isAlreadyStarted()) {
 			response.onAlreadyStarted(request.getGame());
 			return;
 		}
-			
+
 		start();
 		response.onSuccessfullyStarted(request.getGame());
 	}
-	
+
+	private void initializeGame() {
+		game = gameGateway.findGameByName(request.getGame());
+	}
+
 	private void start() {
-		Game game = gameGateway.findGameByName(request.getGame());
 		game.start();
 	}
-	
+
 	private boolean noPermission() {
 		return !permissionGateway.hasPermission(request.getPlayer(), Permissions.START_GAME);
 	}
-	
+
 	private boolean noSuchGame() {
 		return !gameGateway.containsGame(request.getGame());
 	}
-	
+
 	private boolean isAlreadyStarted() {
-		Game game = gameGateway.findGameByName(request.getGame());
 		return game.getGameState().getClass() != StoppedGameState.class;
 	}
-	
+
 	private void setRequest(StartGameRequest request) {
 		this.request = request;
 	}
