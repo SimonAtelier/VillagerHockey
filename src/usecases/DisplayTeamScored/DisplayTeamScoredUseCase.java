5a32671af5adc14fb9ec1f0a1406e3ae02b48c01
Simon Dietz <simon_atelier@web.de>
2018-02-17 10:23:58 +0100

    Refactoring. Extracted methods.

--- src/usecases/DisplayTeamScored/DisplayTeamScoredUseCase.java
+++ src/usecases/DisplayTeamScored/DisplayTeamScoredUseCase.java
@@ -10,25 +10,50 @@ import gateways.GameGateway;
 
 public class DisplayTeamScoredUseCase implements DisplayTeamScored {
 
+	private Game game;
 	private GameGateway gameGateway;
+	private DisplayTeamScoredRequest request;
+	private DisplayTeamScoredResponse response;
 	
 	@Override
 	public void execute(DisplayTeamScoredRequest request, DisplayTeamScoredResponse response) {
-		Game game = gameGateway.findGameByName(request.getGame());
-		
+		setRequest(request);
+		setResponse(response);
+		initializeGame();
+		sendResponse();
+	}
+	
+	private void sendResponse() {
 		List<UUID> viewers = game.getUniquePlayerIds();
 		response.presentTeamScored(viewers, request.getTeam());
-		
+		response.presentScore(viewers, createResponseItems());		
+	}
+	
+	private List<ScoreResponseItem> createResponseItems() {
 		List<ScoreResponseItem> responseItems = new ArrayList<ScoreResponseItem>();
-		List<Team> teams = game.getTeams().findAllTeams();
-		for (Team team : teams) {
+		for (Team team : findAllTeamsOfGame()) {
 			ScoreResponseItem responseItem = new ScoreResponseItem();
 			responseItem.setTeam(team.getName());
 			responseItem.setScore(team.getScore());
 			responseItems.add(responseItem);
 		}
-		
-		response.presentScore(viewers, responseItems);		
+		return responseItems;
+	}
+	
+	private List<Team> findAllTeamsOfGame() {
+		return game.getTeams().findAllTeams();
+	}
+	
+	private void initializeGame() {
+		game = gameGateway.findGameByName(request.getGame());
+	}
+	
+	private void setRequest(DisplayTeamScoredRequest request) {
+		this.request = request;
+	}
+	
+	private void setResponse(DisplayTeamScoredResponse response) {
+		this.response = response;
 	}
 
 	@Override
