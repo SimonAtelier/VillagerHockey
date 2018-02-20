45ac007b7b868b4a6ac4667040ef73387ba153a9
Simon Dietz <simon_atelier@web.de>
2018-02-20 23:16:35 +0100

    Implemented MapTitleConfiguration interface.

---
+++ src/gateways/configuration/MapTitleConfiguration.java
@@ -0,0 +1,19 @@
+package gateways.configuration;
+
+public interface MapTitleConfiguration {
+
+public boolean isWinTitleEnabled();
+	
+	public int getMapTitleFadeInTimeInSeconds();
+	
+	public int getMapTitleFadeOutTimeInSeconds();
+	
+	public int getMapTitleStayTimeInSeconds();
+	
+	public int getMapSubtitleFadeInTimeInSeconds();
+	
+	public int getMapSubtitleFadeOutTimeInSeconds();
+	
+	public int getMapSubtitleStayTimeInSeconds();
+	
+}
