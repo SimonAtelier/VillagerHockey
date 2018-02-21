ae1ca1f17580ac03301e00ac36e3726bdaa88d77
Simon Dietz <simon_atelier@web.de>
2018-02-21 20:58:48 +0100

    Fixed unregister listener.

--- src/usecases/selectteam/SelectTeamViewImpl.java
+++ src/usecases/selectteam/SelectTeamViewImpl.java
@@ -9,6 +9,7 @@ import org.bukkit.Color;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
@@ -55,7 +56,7 @@ public class SelectTeamViewImpl implements SelectTeamView, Listener {
 			return;
 		if (!e.getInventory().equals(inventory))
 			return;
-		InventoryClickEvent.getHandlerList().unregister(this);
+		HandlerList.unregisterAll(this);
 	}
 
 	@Override
