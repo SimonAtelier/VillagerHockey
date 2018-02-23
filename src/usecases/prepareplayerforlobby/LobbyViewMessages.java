a3d2503bd28a492640451d1453a0b06d3d929202
Simon Dietz <simon_atelier@web.de>
2018-02-23 15:07:54 +0100

    Removed message codes from view constants.

--- src/usecases/prepareplayerforlobby/LobbyViewMessages.java
+++ src/usecases/prepareplayerforlobby/LobbyViewMessages.java
@@ -1,15 +1,13 @@
 package usecases.prepareplayerforlobby;
 
-import view.message.MessageCodes;
-
 public interface LobbyViewMessages {
 
-	static final String LOBBY_SELECT_TEAM_DISPLAY_NAME = MessageCodes.YELLOW + "Teamauswahl (Rechtsklick)";
+	static final String LOBBY_SELECT_TEAM_DISPLAY_NAME = "Teamauswahl (Rechtsklick)";
 	
-	static final String LOBBY_LEAVE_GAME_DISPLAY_NAME =  MessageCodes.YELLOW + "Spiel verlassen (Rechtsklick)";
+	static final String LOBBY_LEAVE_GAME_DISPLAY_NAME =  "Spiel verlassen (Rechtsklick)";
 	
-	static final String LOBBY_ACHIEVEMENTS_DISPLAY_NAME =  MessageCodes.YELLOW + "Achievements (Rechtsklick)";
+	static final String LOBBY_ACHIEVEMENTS_DISPLAY_NAME =  "Achievements (Rechtsklick)";
 	
-	static final String LOBBY_FORCE_START_DISPLAY_NAME =  MessageCodes.YELLOW + "Start erzwingen (Rechtsklick)";
+	static final String LOBBY_FORCE_START_DISPLAY_NAME =  "Start erzwingen (Rechtsklick)";
 	
 }
