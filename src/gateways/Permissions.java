2396d0266594cd002572dfae4813e1bd9e48f854
Simon Dietz <simon_atelier@web.de>
2018-02-23 13:23:37 +0100

    Implemented PermissionsGateway. Changed Permission values.

--- src/gateways/Permissions.java
+++ src/gateways/Permissions.java
@@ -2,44 +2,44 @@ package gateways;
 
 public interface Permissions {
 
-	static final String ADD_GAME = "setup";
+	static final String ADD_GAME = "vh.setup";
 	
-	static final String JOIN_GAME = "base";
+	static final String JOIN_GAME = "vh.base";
 	
-	static final String KICK_PLAYER = "admin";
+	static final String KICK_PLAYER = "vh.admin";
 	
-	static final String LIST_GAMES = "setup";
+	static final String LIST_GAMES = "vh.setup";
 	
-	static final String SAVE_GAME = "setup";
+	static final String SAVE_GAME = "vh.setup";
 	
-	static final String SET_GOAL = "setup";
+	static final String SET_GOAL = "vh.setup";
 	
-	static final String SET_LOBBY = "setup";
+	static final String SET_LOBBY = "vh.setup";
 	
-	static final String ADD_TEAM_SPAWN = "setup";
+	static final String ADD_TEAM_SPAWN = "vh.setup";
 	
-	static final String SET_VILLAGER_SPAWN = "setup";
+	static final String SET_VILLAGER_SPAWN = "vh.setup";
 	
-	static final String PLACE_JOIN_SIGN = "setup";
+	static final String PLACE_JOIN_SIGN = "vh.setup";
 	
-	static final String SHOW_HELP = "base";
+	static final String SHOW_HELP = "vh.base";
 	
-	static final String JOIN_TEAM = "base";
+	static final String JOIN_TEAM = "vh.base";
 	
-	static final String SET_MIN_PLAYERS = "setup";
+	static final String SET_MIN_PLAYERS = "vh.setup";
 	
-	static final String SET_PLAYING_TIME = "setup";
+	static final String SET_PLAYING_TIME = "vh.setup";
 	
-	static final String ADD_TEAM = "setup";
+	static final String ADD_TEAM = "vh.setup";
 	
-	static final String REMOVE_JOIN_SIGN = "setup";
+	static final String REMOVE_JOIN_SIGN = "vh.setup";
 	
-	static final String REMOVE_GAME = "setup";
+	static final String REMOVE_GAME = "vh.setup";
 	
-	static final String START_GAME = "setup";
+	static final String START_GAME = "vh.setup";
 	
-	static final String STOP_GAME = "setup";
+	static final String STOP_GAME = "vh.setup";
 	
-	static final String FORCE_START = "vip.forcestart";
+	static final String FORCE_START = "vh.vip.forcestart";
 	
 }
