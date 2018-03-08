befe1332bba7e7ac8af2d8247024bda59602d18b
Simon <Simon@LAPTOP-NAT2JJES.Speedport_W_921V_1_44_000>
2018-03-08 10:53:17 +0100

    Changed implementation to not use deprecated class.

--- src/usecases/performaction/pickupitem/PickupItemEventListener.java
+++ src/usecases/performaction/pickupitem/PickupItemEventListener.java
@@ -1,15 +1,21 @@
 package usecases.performaction.pickupitem;
 
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerPickupItemEvent;
+import org.bukkit.event.entity.EntityPickupItemEvent;
 
 public class PickupItemEventListener implements Listener {
 
 	@EventHandler
-	public void onPlayerPickupItem(PlayerPickupItemEvent e) {
+	public void onEntityPickupItem(EntityPickupItemEvent e) {
+		if (e.getEntityType() != EntityType.PLAYER)
+			return;
+		
+		Player player = (Player) e.getEntity();
 		PickupItem useCase = new PickupItemUseCase();
-		e.setCancelled(!useCase.canPickupItem(e.getPlayer().getUniqueId()));
+		e.setCancelled(!useCase.canPickupItem(player.getUniqueId()));
 	}
 	
 }
