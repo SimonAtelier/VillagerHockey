0a522d0286395f20eb66e9690069af5199b6f1e4
SimonAtelier <simon_atelier@web.de>
2021-05-06 07:49:08 +0200

    Refactoring extracted methods.

--- src/usecases/jointeam/JoinTeamController.java
+++ src/usecases/jointeam/JoinTeamController.java
@@ -10,23 +10,67 @@ import view.teamarmour.ColoredTeamArmourViewImpl;
 
 public class JoinTeamController implements TeamSelectListener {
 	
+	private UUID player;
+	private String game;
+	private String team;
+	
 	@Override
-	public void onTeamSelected(UUID player, String game, String team) {			
-		JoinTeamRequestModel requestModel = new JoinTeamRequestModel();
-		requestModel.setGame(game);
-		requestModel.setTeam(team);
-		requestModel.setPlayer(player);
-		
-		ColoredTeamArmourView teamArmourView = new ColoredTeamArmourViewImpl();
-		JoinTeamView view = new JoinTeamViewImpl(player);
-		
-		JoinTeamResponse presenter = new JoinTeamPresenter(view, teamArmourView);
-		
+	public void onTeamSelected(UUID player, String game, String team) {		
+		setPlayer(player);
+		setGame(game);
+		setTeam(team);
+		executeUseCase();
+	}
+	
+	private void executeUseCase() {
+		createUseCase().execute(createRequestModel(), createResponse());
+	}
+	
+	private JoinTeam createUseCase() {
 		JoinTeam useCase = new JoinTeamUseCase();
 		useCase.setGameGateway(Context.gameGateway);
 		useCase.setPermissionGateway(Context.permissionGateway);
 		useCase.setPlayerGateway(Context.playerGateway);
-		useCase.execute(requestModel, presenter);
+		return useCase;
 	}
-		
+	
+	private JoinTeamResponse createResponse() {
+		ColoredTeamArmourView teamArmourView = new ColoredTeamArmourViewImpl();
+		JoinTeamView view = new JoinTeamViewImpl(getPlayer());
+		JoinTeamResponse presenter = new JoinTeamPresenter(view, teamArmourView);
+		return presenter;
+	}
+	
+	private JoinTeamRequestModel createRequestModel() {
+		JoinTeamRequestModel requestModel = new JoinTeamRequestModel();
+		requestModel.setGame(getGame());
+		requestModel.setTeam(getTeam());
+		requestModel.setPlayer(getPlayer());
+		return requestModel;
+	}
+
+	private UUID getPlayer() {
+		return player;
+	}
+
+	private void setPlayer(UUID player) {
+		this.player = player;
+	}
+
+	private String getGame() {
+		return game;
+	}
+
+	private void setGame(String game) {
+		this.game = game;
+	}
+
+	private String getTeam() {
+		return team;
+	}
+
+	private void setTeam(String team) {
+		this.team = team;
+	}
+		 
 }
