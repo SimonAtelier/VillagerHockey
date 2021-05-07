43c6cefe88203a1f21240c9e95dbbf18bc27bb8f
SimonAtelier <simon_atelier@web.de>
2021-05-07 15:44:14 +0200

    Changed messages.

--- src/usecases/prepareplayerforlobby/LobbyViewMessages.java
+++ src/usecases/prepareplayerforlobby/LobbyViewMessages.java
@@ -2,14 +2,14 @@ package usecases.prepareplayerforlobby;
 
 public interface LobbyViewMessages {
 
-	static final String LOBBY_SELECT_TEAM_DISPLAY_NAME = "Teamauswahl (Rechtsklick)";
+	static final String LOBBY_SELECT_TEAM_DISPLAY_NAME = "§rTeamauswahl (Rechtsklick)";
 	
-	static final String LOBBY_LEAVE_GAME_DISPLAY_NAME =  "Spiel verlassen (Rechtsklick)";
+	static final String LOBBY_LEAVE_GAME_DISPLAY_NAME =  "§rSpiel verlassen (Rechtsklick)";
 	
-	static final String LOBBY_ACHIEVEMENTS_DISPLAY_NAME =  "Achievements (Rechtsklick)";
+	static final String LOBBY_ACHIEVEMENTS_DISPLAY_NAME =  "§rAchievements (Rechtsklick)";
 	
-	static final String LOBBY_FORCE_START_DISPLAY_NAME =  "Start erzwingen (Rechtsklick)";
+	static final String LOBBY_FORCE_START_DISPLAY_NAME =  "§rStart erzwingen (Rechtsklick)";
 	
-	static final String LOBBY_COSMETICS_DISPLAY_NAME = "Cosmetics (Rechtsklick)";
+	static final String LOBBY_COSMETICS_DISPLAY_NAME = "§rCosmetics (Rechtsklick)";
 	
 }
