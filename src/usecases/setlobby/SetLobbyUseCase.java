e598d79e8152778c8bc57d79b7695a68d7e48dca
SimonAtelier <simon_atelier@web.de>
2021-05-08 12:50:40 +0200

    Refactoring extracted methods.

--- src/usecases/setlobby/SetLobbyUseCase.java
+++ src/usecases/setlobby/SetLobbyUseCase.java
@@ -9,29 +9,42 @@ import gateways.Permissions;
 public class SetLobbyUseCase implements SetLobby {
 	
 	private SetLobbyRequest request;
+	private SetLobbyResponse response;
 	private GameGateway gameGateway;
 	private PermissionGateway permissionGateway;
 	
 	@Override
 	public void execute(SetLobbyRequest request, SetLobbyResponse response) {
 		setRequest(request);
+		setResponse(response);
 		
 		if (noPermission()) {
-			response.onNoPermission();
+			sendNoPermissionResponse();
 			return;
 		}
 
 		if (noSuchGame()) {
-			response.onNoGameWithSuchName(request.getGame());
+			sendNoGameWithSuchNameResponse();
 			return;
 		}
 		
 		setLobby();
-		
-		response.onLobbySuccessfullySet(request.getGame());
+		sendSuccessResponse();
 	}
 	
-	private Location createLocationFromRequest(SetLobbyRequest request) {
+	private void sendNoGameWithSuchNameResponse() {
+		getResponse().onNoGameWithSuchName(getGameNameFromRequest());
+	}
+	
+	private void sendSuccessResponse() {
+		getResponse().onLobbySuccessfullySet(getGameNameFromRequest());
+	}
+	
+	private void sendNoPermissionResponse() {
+		getResponse().onNoPermission();
+	}
+	
+	private Location createLocationFromRequest() {
 		Location location = new Location();
 		location.setX(request.getX());
 		location.setY(request.getY());
@@ -42,21 +55,37 @@ public class SetLobbyUseCase implements SetLobby {
 		return location;
 	}
 	
+	private String getGameNameFromRequest() {
+		return getRequest().getGame();
+	}
+	
 	private boolean noSuchGame() {
-		return !gameGateway.containsGame(request.getGame());
+		return !gameGateway.containsGame(getGameNameFromRequest());
 	}
 	
 	private boolean noPermission() {
-		return !permissionGateway.hasPermission(request.getPlayer(), Permissions.SET_LOBBY);
+		return !permissionGateway.hasPermission(getRequest().getPlayer(), Permissions.SET_LOBBY);
+	}
+	
+	private void setLobby() {
+		Game game = gameGateway.findGameByName(getGameNameFromRequest());
+		game.setLobby(createLocationFromRequest());
+	}
+
+	private SetLobbyResponse getResponse() {
+		return response;
+	}
+
+	private void setResponse(SetLobbyResponse response) {
+		this.response = response;
+	}
+	
+	private SetLobbyRequest getRequest() {
+		return request;
 	}
 	
 	private void setRequest(SetLobbyRequest request) {
 		this.request = request;
-	}
-	
-	private void setLobby() {
-		Game game = gameGateway.findGameByName(request.getGame());
-		game.setLobby(createLocationFromRequest(request));
 	}
 
 	@Override
