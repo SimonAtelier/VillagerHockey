5fe0af9a03502de7be43107d3aeece42409940c6
SimonAtelier <simon_atelier@web.de>
2021-05-15 22:17:59 +0200

    Removed not needed setters.

--- src/main/java/achievements/Achievement.java
+++ src/main/java/achievements/Achievement.java
@@ -53,34 +53,18 @@ public class Achievement {
 		return points;
 	}
 
-	public void setPoints(int points) {
-		this.points = points;
-	}
-
 	public String getId() {
 		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
 	}
 
 	public String getName() {
 		return name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
 	public String getDescription() {
 		return description;
 	}
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	
 	public void setProgress(String key, boolean progress) {
 		for (AchieveCondition condition : achieveConditions) {
 			if (condition.getPropertyKey().equals(key))
